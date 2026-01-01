8c4e10eacb8a4c13f7a0f0af6f688a681e45d0c6
Saúl Sáyago <sayagoadd@gmail.com>
2025-12-31 19:04:10 -0600

    style: fix x padding in /events

--- src/app/(private)/events/page.tsx
+++ src/app/(private)/events/page.tsx
@@ -20,38 +20,36 @@ export default async function EventsPage() {
 
   return (
     <>
-      <Container>
-        <div className="flex gap-4 items-baseline">
-          <h1 className="text-3xl lg:text-4xl xl:text-5xl font-semibold mb-6">
-            Eventos
-          </h1>
-          <Button asChild>
+      <div className="flex gap-4 items-baseline">
+        <h1 className="text-3xl lg:text-4xl xl:text-5xl font-semibold mb-6">
+          Eventos
+        </h1>
+        <Button asChild>
+          <Link href="/events/new" className="text-lg">
+            <CalendarPlus className="size-5" />
+            Nuevo Evento
+          </Link>
+        </Button>
+      </div>
+      {events.length > 0 ? (
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {events.map((event) => (
+            <EventCard key={event.id} {...event} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col items-center gap-4">
+          <CalendarRange className="size-16 mx-auto" />
+          No tienes ningún evento creado aún. Crea uno para empezar a agendar
+          citas.
+          <Button size="lg" className="text-lg" asChild>
             <Link href="/events/new" className="text-lg">
               <CalendarPlus className="size-5" />
               Nuevo Evento
             </Link>
           </Button>
         </div>
-        {events.length > 0 ? (
-          <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            {events.map((event) => (
-              <EventCard key={event.id} {...event} />
-            ))}
-          </div>
-        ) : (
-          <div className="flex flex-col items-center gap-4">
-            <CalendarRange className="size-16 mx-auto" />
-            No tienes ningún evento creado aún. Crea uno para empezar a agendar
-            citas.
-            <Button size="lg" className="text-lg" asChild>
-              <Link href="/events/new" className="text-lg">
-                <CalendarPlus className="size-5" />
-                Nuevo Evento
-              </Link>
-            </Button>
-          </div>
-        )}
-      </Container>
+      )}
     </>
   );
 }
